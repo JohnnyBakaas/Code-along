167ade994d7c9f6fa9450a9a702989b834ab7a3b
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-11-03 13:31:29 +0100

    Co-authored-by: HeineTL <HeineTL@users.noreply.github.com> Co-authored-by: RebeckaKinn <RebeckaKinn@users.noreply.github.com> Co-authored-by: Kinvalan <Kinvalan@users.noreply.github.com> Co-authored-by: andeypandey <andeypandey@users.noreply.github.com> Co-authored-by: ftveiten <ftveiten@users.noreply.github.com> Co-authored-by: Ståle <Unixan@users.noreply.github.com> Co-authored-by: istensli <istensli@users.noreply.github.com> Co-authored-by: Nikolaipe <Nikolaipe@users.noreply.github.com> Co-authored-by: armful <a@soda.bot> Co-authored-by: fadiGhadri <fadiGhadri@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,38 @@
+//
+
+let log = "";
+
+let lotteryOne = `Kake`;
+let lotteryTwo = `Sydentur`;
+let lotteryThree = `Ett gratis måltid på McDonald's`;
+let lotteryFour = `First Price Cookies`;
+//4 innhold /\
+
+//random-funksjon
+function randomOut(designatedDiv) {
+  let svar = "";
+
+  if (designatedDiv.innerHTML != "") return;
+
+  svar = Math.floor(Math.random() * 4) + 1;
+
+  console.log(svar);
+
+  //lagre svaret
+  log += svar;
+
+  designatedDiv.innerHTML = numberToAnswer(svar);
+  winner();
+}
+
+function numberToAnswer(randomNumber) {
+  if (randomNumber == 1) {
+    return lotteryOne;
+  } else if (randomNumber == 2) {
+    return lotteryTwo;
+  } else if (randomNumber == 3) {
+    return lotteryThree;
+  } else if (randomNumber == 4) {
+    return lotteryFour;
+  }
+}
